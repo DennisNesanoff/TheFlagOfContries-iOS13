54efc6a967f62ca0111dc45e4a4ffa3f95390135
Dennis <nndenninn@gmail.com>
2020-03-09 14:05:33 +0300

    Initial commit

---
+++ TheFlagOfContries-iOS13/MainTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainTableViewController.swift
+//  TheFlagOfContries-iOS13
+//
+//  Created by Dennis Nesanoff on 09.03.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    var countries = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Contries"
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].uppercased()
+        cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
